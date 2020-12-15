7645123bc2a6c31d6dc6544d4188c54f1b761f03
tloeber <thomas.loeber73@gmail.com>
2020-12-15 18:28:17 -0500

    Add theme.

--- hugo/create_new_site.sh
+++ hugo/create_new_site.sh
@@ -1,6 +1,11 @@
 hugo_version="0.79.0"
+theme=hugo-future-imperfect
 
 docker container run --rm -it \
   -v $(pwd)/src:/src \
   klakegg/hugo:$hugo_version \
   new site blog6
+
+# Copy theme using hard links.Theme already has to be cloned
+# into the shared themes folder.
+ cp -lr themes/${theme} ./src/
