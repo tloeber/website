459d16ad4a990bada118cee043fcaed7cafa386c
tloeber <thomas.loeber73@gmail.com>
2020-12-16 20:34:48 -0500

    Reorganize directory structure.

--- create_new_site.sh
+++ create_new_site.sh
@@ -1,12 +1,15 @@
 hugo_version="0.79.0"
 theme="hugo-future-imperfect"
-site_name="blog"
 
+# So that relative paths work
+cd ~/projects/website
+
+mkdir src
 docker container run --rm -it \
   -v $(pwd)/src:/src \
   klakegg/hugo:${hugo_version} \
-  new site $site_name
+  new site .
 
 # Copy theme using hard links.Theme already has to be cloned
 # into the shared themes folder.
- cp -lr themes/${theme} ./src/
+ cp -lr themes/${theme} ./src/${theme}
