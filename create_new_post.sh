1f730de9f08f4647e1d83c70f4017a80fd051b77
tloeber <thomas.loeber73@gmail.com>
2020-12-18 20:51:21 -0500

    Make user the owner of post after creating it, so they don't have to be root to edit it.

--- create_new_post.sh
+++ create_new_post.sh
@@ -10,3 +10,7 @@ docker container run --rm -it \
   -v $(pwd)/src:/src \
   klakegg/hugo:${hugo_version} \
   new "${section_name}/${post_name}.${format}"
+
+# Give user write access to content, so that they can 
+# easily edit markdown files directly.
+ sudo chown $(whoami):$(whoami) "src/content/${section_name}/${post_name}.${format}"
