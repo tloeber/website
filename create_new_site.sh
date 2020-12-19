cb433f04220e431ea0926c76a0cd4a925c1ad6b7
tloeber <thomas.loeber73@gmail.com>
2020-12-18 20:50:55 -0500

    Add additional comments..

--- create_new_site.sh
+++ create_new_site.sh
@@ -1,15 +1,17 @@
 hugo_version="0.79.0"
 theme="hugo-future-imperfect"
 
-# So that relative paths work
+# To make sure that relative paths work
 cd ~/projects/website
 
+# Create directory where applicatiion files will be located.
+# Note that the name "src" is hardcoded into Klakegg's docker image.
 mkdir src
 docker container run --rm -it \
   -v $(pwd)/src:/src \
   klakegg/hugo:${hugo_version} \
   new site .
 
-# Copy theme using hard links.Theme already has to be cloned
-# into the shared themes folder.
- cp -lr themes/${theme} ./src/${theme}
+# Copy theme using hard links.Theme already has to be cloned 
+# into a shared themes folder.
+ cp -lr themes/${theme} src/${theme}
