8939d047b5dc3e742476bb9b238c2efb941ec861
tloeber <thomas.loeber73@gmail.com>
2021-01-08 10:10:15 -0500

    Automatically create subdirectories if any don't exist yet.

--- content/create_header_template.sh
+++ content/create_header_template.sh
@@ -1,25 +1,33 @@
 hugo_version="0.79.0"
-file_names=("blog/header_template_blog.md" "header_template_general.md")
+file_name="header_template.md"
+prefixes=("." "blog")
 
 # So that relative paths work
 cd ~/projects/website
 
 # The automatically created headers differ based on prefix 
-# (e.g., blog). That's why we grade separate template header for each.
-for fn in "${file_names[@]}"; do
-  #temp
-  rm -f "src/content/${fn}" 
-  rm -f "content/${fn}" 
+# (e.g., blog). That's why we create separate template header for each.
+for prefix in "${prefixes[@]}"; do
+  relative_path="${prefix}/${file_name}"
 
+  # Remove previous versions, if any
+  rm -f "src/content/${relative_path}" 
+  rm -f "content/${relative_path}" 
+
+  # This creates the header template
   docker container run --rm -it \
     -v $(pwd)/src:/src \
     klakegg/hugo:${hugo_version} \
-    new "$fn"
+    new "$relative_path"
     
-  # Store header template in permanent directory
-  sudo mv "src/content/${fn}" "content/${fn}"
+  ## Store header template in *permanent* content directory
+  # Create subdirectory first, if it doesn't exist
+  if [ ! -d "content/${prefix}" ]; then
+    mkdir -p "content/${prefix}";
+  fi
+  sudo mv "src/content/${relative_path}" "content/${relative_path}"
 
   # Give user write access to content.
-  sudo chown $(whoami):$(whoami) "content/${fn}"
+  sudo chown $(whoami):$(whoami) "content/${relative_path}"
 
 done
