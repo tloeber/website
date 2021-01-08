87e3276b0554b4221ee7ea21474917110b769e8c
tloeber <thomas.loeber73@gmail.com>
2021-01-08 09:47:20 -0500

    Allow creating multiple separate header files for different prefixes (e.g., blog), since these have different default headers.

--- content/create_header_template.sh
+++ content/create_header_template.sh
@@ -1,21 +1,25 @@
 hugo_version="0.79.0"
-file_name="blog/header_template.md"
+file_names=("blog/header_template_blog.md" "header_template_general.md")
 
 # So that relative paths work
 cd ~/projects/website
 
-#temp
-rm -f src/content/"${file_name}" 
-rm -f content/"${file_name}" 
+# The automatically created headers differ based on prefix 
+# (e.g., blog). That's why we grade separate template header for each.
+for fn in "${file_names[@]}"; do
+  #temp
+  rm -f "src/content/${fn}" 
+  rm -f "content/${fn}" 
 
+  docker container run --rm -it \
+    -v $(pwd)/src:/src \
+    klakegg/hugo:${hugo_version} \
+    new "$fn"
+    
+  # Store header template in permanent directory
+  sudo mv "src/content/${fn}" "content/${fn}"
 
-docker container run --rm -it \
-  -v $(pwd)/src:/src \
-  klakegg/hugo:${hugo_version} \
-  new "$file_name"
-  
-# Store header template in permanent directory
-sudo mv src/content/"${file_name}" content/"${file_name}"
+  # Give user write access to content.
+  sudo chown $(whoami):$(whoami) "content/${fn}"
 
-# Give user write access to content.
- sudo chown $(whoami):$(whoami) content/"${file_name}"
+done
