24c1ca121208be923b0a647a16bea0fc1024d395
tloeber <thomas.loeber73@gmail.com>
2021-06-18 09:18:43 +0200

    Rearrange commands to improve readability.

--- create_new_site.sh
+++ create_new_site.sh
@@ -1,7 +1,3 @@
-# TEMPORARY: Delete old version
-sudo rm -rf src
-# =================================================================
-
 hugo_version="0.79.1"
 theme="hugo-future-imperfect" 
 
@@ -10,24 +6,27 @@ cd ~/projects/website
 
 # Create directory where applicatiion files will be located.
 # Note that the name "src" is hardcoded into Klakegg's docker image.
-# Eventually we want to move to a docker volume instead.
-mkdir src
+# TODO: move to a docker volume instead.
+# TEMPORARY: Delete old version
+sudo rm -rf src && \
+  mkdir src  # rm -rf src/* wouldn't delete hidden files
 docker container run --rm -it \
   -v $(pwd)/src:/src \
   klakegg/hugo:${hugo_version} \
   new site .
 
-# Copy theme from the themes folder in the base directory (where themes are
+# Copy theme from  themes/ in the base directory (where themes are
 # permanently stored so they don't have to be re-downloaded) to the docker 
-# volume/bind mount (src/themes). Don't copy themes/exampleSite. 
-# TODO: Find out if any other subfolders of themes don't need to be copied.
+# volume/bind mount (src/themes). 
+# Copy individual directory so we can exclude themes/exampleSite. 
+# TODO: Find out if any other subfolders of themes/ can be excluded too.
 sudo mkdir src/themes/${theme} && \
   cd themes/${theme} && \
   sudo cp -r archetypes  images  layouts  LICENSE.md  README.md  static  theme.toml  ../../src/themes/${theme} && \
   cd ../..
 
-# This creates config by combining a template with content (separated to distinguish
-# proper config changes from content changes).
+# This creates config by combining a template with content.
+# (Separated to distinguish config changes from content changes).
 bash configs/create_config.sh | sudo tee src/config.toml > /dev/null
 
 # Create templates for content headers
